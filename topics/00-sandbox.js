4e0494e097b451871720c4321e9373949a56e2f3
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-08-27 19:50:36 +0200

    JSON

--- topics/00-sandbox.js
+++ topics/00-sandbox.js
@@ -9,10 +9,51 @@
   }
 }
 
-//DEMO-1a -------------------------------------
+//DEMO-2a -------------------------------------
 
 {
   let x = 15 * 5;
-  debugger;
+  /*   debugger; */
   document.getElementById("demo_2a").innerHTML = x;
+}
+
+//DEMO-3a -------------------------------------
+
+{
+  const x7 = function (x) {
+    return 2 * x;
+  };
+
+  document.getElementById("demo_3a").innerHTML = x7(16);
+}
+
+{
+  let day;
+
+  switch (/* new Date().getDay() */ 8) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      day = "unknown";
+  }
+  document.getElementById("demo_4a").innerHTML = "Today is " + day;
 }
