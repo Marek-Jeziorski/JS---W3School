448bb0519df844d279bafb2d26766cc2bfaa6796
Marek-Jeziorski <111072844+Marek-Jeziorski@users.noreply.github.com>
2022-08-28 19:28:15 +0200

    OBJECT-ADV

--- topics/07a-object.js
+++ topics/07a-object.js
@@ -1,1 +1,27 @@
-//-----------------------------------------
+// DEMO_6A -----------------------------------------
+{
+  const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue",
+  };
+
+  const x = person; // niech x będzie obiektem {person} (przekazujemy odniesienie do obiektu)
+  x.age = 10;
+
+  document.getElementById("demo_6a").innerHTML =
+    person.firstName + " is " + person.age + " years old.";
+}
+
+// DEMO_6B -----------------------------------------
+{
+  let person = "marek";
+
+  let x = person; // niech x będzie miał wartość 'person' (pekazujemy wartość zmiennej person)
+
+  x = "Anna";
+
+  document.getElementById("demo_6b").innerHTML =
+    " person = : " + person + "<br> x = : " + x;
+}
